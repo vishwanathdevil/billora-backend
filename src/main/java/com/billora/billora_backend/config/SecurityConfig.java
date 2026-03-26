09b1a09a8db7ce1d9140a0599b010406fb2e85cc
vishwanathdevil <156284668+vishwanathdevil@users.noreply.github.com>
2026-03-26 21:30:49 +0530

    disable security

--- src/main/java/com/billora/billora_backend/config/SecurityConfig.java
+++ src/main/java/com/billora/billora_backend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.billora.billora_backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,13 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(csrf -> csrf.disable()) // disable csrf
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+                .requestMatchers("/**").permitAll() // allow all APIs
+            )
+            .httpBasic(Customizer.withDefaults()); // keep basic config
 
         return http.build();
     }
