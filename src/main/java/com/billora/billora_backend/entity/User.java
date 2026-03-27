5c5ee90bc6491e0053be297caba0a01e613384c7
vishwanathdevil <156284668+vishwanathdevil@users.noreply.github.com>
2026-03-27 13:56:51 +0530

    fix column case issue

--- src/main/java/com/billora/billora_backend/entity/User.java
+++ src/main/java/com/billora/billora_backend/entity/User.java
@@ -10,7 +10,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username")   // 🔥 IMPORTANT FIX
+    @Column(name = "\"username\"")   // 🔥 IMPORTANT FIX
     private String username;
 
     @Column(name = "password")   // 🔥 IMPORTANT FIX
