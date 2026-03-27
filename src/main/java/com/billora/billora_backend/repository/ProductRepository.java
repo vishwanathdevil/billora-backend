ae82797c054715883ecb903e1b991b3031d9fcaa
vishwanathdevil <156284668+vishwanathdevil@users.noreply.github.com>
2026-03-27 19:22:02 +0530

    final product scan fix

--- src/main/java/com/billora/billora_backend/repository/ProductRepository.java
+++ src/main/java/com/billora/billora_backend/repository/ProductRepository.java
@@ -2,11 +2,10 @@ package com.billora.billora_backend.repository;
 
 import com.billora.billora_backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE p.code LIKE %:code%")
-    Product findByCode(@Param("code") String code);
+    
+    Product findByCode(String code);
 }
