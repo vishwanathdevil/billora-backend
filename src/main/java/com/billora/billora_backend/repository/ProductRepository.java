73e748441706768b7d3a16fd53e2f238a807a37b
vishwanathdevil <156284668+vishwanathdevil@users.noreply.github.com>
2026-03-27 15:53:42 +0530

    fixed barcode matching

--- src/main/java/com/billora/billora_backend/repository/ProductRepository.java
+++ src/main/java/com/billora/billora_backend/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.billora.billora_backend.repository;
 
 import com.billora.billora_backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findByCode(String code);
+    @Query("SELECT p FROM Product p WHERE p.code LIKE %:code%")
+    Product findByCode(@Param("code") String code);
 }
